273e905da71da94c5fb7d9c5b3c954802eb53194
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-02 10:02:08 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
